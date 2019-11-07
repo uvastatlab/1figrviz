a2006b41ef003f754326c6cb0c85ad5f52546a97
Clay Ford <joe.clayton.ford@gmail.com>
2019-11-07 10:44:39 -0500

    add cleveland dot plots for els sub vs free

--- R/figure7.R
+++ R/figure7.R
@@ -18,78 +18,47 @@ journal <- readRDS("1science/journal.rds")
 # ----------------------------------------------
 # keep only elsevier
 els <- journal %>% 
-  select(journal, provider, type, jr1, jr5, cites, pubs, synth, scopus, domain, field, jid) %>% 
+  select(journal, provider, provider2, type, jr1, jr5, cites, pubs, domain) %>% 
   filter(type == "Journal" & provider == "Elsevier") %>% 
   mutate(jr1 = as.integer(jr1), jr5 = as.integer(jr5))
 
 # group/summarize by domain
 els_group <- els %>% 
   group_by(domain) %>% 
-  summarize(titles = n(),
-            titlesjr1 = sum(!is.na(jr1)),
-            titlesjr5 = sum(!is.na(jr5)),
-            jr1 = sum(jr1, na.rm = TRUE),
+  summarize(jr1 = sum(jr1, na.rm = TRUE),
             jr5 = sum(jr5, na.rm = TRUE),
             refs = sum(cites, na.rm = TRUE),
             pubs = sum(pubs, na.rm = TRUE)) %>% 
   mutate(domain = factor(domain))
 
 
+# plot function
+plot_fn <- function(y, title){
+  y <- enquo(y)
+  els_group %>% 
+    mutate(domain = fct_reorder(domain, !! y)) %>% 
+    ggplot(aes(x = domain, y = !! y, color = domain)) + 
+    geom_segment(aes(x = domain, xend = domain, y = 0, yend = !! y), color = "grey", size = 1) +
+    geom_point(size = 5) +
+    scale_y_continuous(labels = scales::comma) +
+    scale_color_brewer(palette = "BuPu", direction = 1) +
+    labs(title = title,
+         subtitle = "By Article Domain",
+         x = "", y = "Number of Downloads") +
+    geom_text(aes(label = comma(!! y)), nudge_x = -.25, color = "black", size = 3) +
+    theme(legend.position = "none") +
+    coord_flip() 
+}
+
+
 # plot jr1 by domain
-els_group %>% 
-  mutate(domain = fct_reorder(domain, jr1)) %>% 
-  ggplot(aes(x = domain, y = jr1, color = domain)) + 
-  geom_segment(aes(x = domain, xend = domain, y = 0, yend = jr1), color = "grey", size = 1) +
-  geom_point(size = 5) +
-  scale_color_brewer(palette = "BuPu", direction = 1) +
-  labs(title = "All 2017 Downloads from Elsevier Journals",
-       subtitle = "By Article Domain",
-       x = "", y = "Number of Downloads") +
-  geom_text(aes(label = comma(jr1)), nudge_x = -.25, color = "black", size = 3) +
-  theme(legend.position = "none") +
-  coord_flip() 
-
+plot_fn(jr1, title = "All 2017 Downloads from Elsevier Journals")
 # plot jr5 by domain
-els_group %>% 
-  mutate(domain = fct_reorder(domain, jr5)) %>% 
-  ggplot(aes(x = domain, y = jr5, color = domain)) + 
-  geom_segment(aes(x = domain, xend = domain, y = 0, yend = jr5), color = "grey", size = 1) +
-  geom_point(size = 5) +
-  scale_color_brewer(palette = "BuPu", direction = 1) +
-  labs(title = "Current Year 2017 Downloads from Elsevier Journals",
-       subtitle = "By Article Domain",
-       x = "", y = "Number of Downloads") +
-  geom_text(aes(label = comma(jr5)), nudge_x = -.25, color = "black", size = 3) +
-  theme(legend.position = "none") +
-  coord_flip() 
-
+plot_fn(jr5, title = "Current Year 2017 Downloads from Elsevier Journals")
 # plot refs by domain
-els_group %>% 
-  mutate(domain = fct_reorder(domain, refs)) %>% 
-  ggplot(aes(x = domain, y = refs, color = domain)) + 
-  geom_segment(aes(x = domain, xend = domain, y = 0, yend = refs), color = "grey", size = 1) +
-  geom_point(size = 5) +
-  scale_color_brewer(palette = "BuPu", direction = 1) +
-  labs(title = "UVA References by in Elsevier Journals",
-       subtitle = "By Article Domain",
-       x = "", y = "Number of Downloads") +
-  geom_text(aes(label = comma(refs)), nudge_x = -.25, color = "black", size = 3) +
-  theme(legend.position = "none") +
-  coord_flip() 
-
+plot_fn(refs, title = "UVA References in Elsevier Journals")
 # plot pubs by domain
-els_group %>% 
-  mutate(domain = fct_reorder(domain, pubs)) %>% 
-  ggplot(aes(x = domain, y = pubs, color = domain)) + 
-  geom_segment(aes(x = domain, xend = domain, y = 0, yend = pubs), color = "grey", size = 1) +
-  geom_point(size = 5) +
-  scale_color_brewer(palette = "BuPu", direction = 1) +
-  labs(title = "UVA-Authored Publications in Elsevier Journals",
-       subtitle = "By Article Domain",
-       x = "", y = "Number of Downloads") +
-  geom_text(aes(label = comma(pubs)), nudge_x = -.25, color = "black", size = 3) +
-  theme(legend.position = "none") +
-  coord_flip() 
+plot_fn(pubs, title = "UVA-Authored Publications in Elsevier Journals")
 
 # Percents
 els_total <- els_group %>% 
@@ -100,3 +69,96 @@ els_total <- els_group %>%
 percents <- els_group$jr1/els_total$jr1
 percents[5]
 
+
+# adding Cleveland dot plots for Els sub vs free
+# source: https://uc-r.github.io/cleveland-dot-plots
+
+# group/summarize by domain and provider2 (Els free and subs)
+# Note: General domain has no data for Elsevier subscribed
+els_group2 <- els %>% 
+  group_by(domain, provider2) %>% 
+  summarize(jr1 = sum(jr1, na.rm = TRUE),
+            jr5 = sum(jr5, na.rm = TRUE),
+            refs = sum(cites, na.rm = TRUE),
+            pubs = sum(pubs, na.rm = TRUE)) %>% 
+  ungroup() %>% 
+  mutate(domain = factor(domain))
+
+pal <- c("#8C96C6", "#6E016B")
+
+
+right_label <- els_group2 %>%
+  select(domain, provider2, jr1) %>% 
+  group_by(domain) %>%
+  arrange(desc(jr1)) %>%
+  top_n(1, wt = jr1)
+
+left_label <- els_group2 %>%
+  select(domain, provider2, jr1) %>% 
+  group_by(domain) %>%
+  arrange(desc(jr1)) %>%
+  slice(2) %>% 
+  arrange(desc(jr1))
+
+
+ymax <- els_group2 %>% summarise(ymax = max(jr1)) %>% pull() * 1.15
+ymin <- els_group2 %>% summarise(ymin = min(jr1)) %>% pull() * -20
+# ymax <- max(els_group2$jr1) * 1.15
+els_group2 %>% 
+  mutate(domain = fct_reorder(domain, jr1)) %>% 
+  ggplot(aes(x = jr1, y = domain)) +
+  geom_line(aes(group = domain)) +
+  geom_point(aes(color = provider2), size = 2) +
+  geom_text(data = right_label, aes(color = provider2, label = scales::comma(jr1)),
+            size = 3, hjust = -.5, show.legend = F) +
+  geom_text(data = left_label, aes(color = provider2, label = scales::comma(jr1)),
+            size = 3, hjust = 1.5, show.legend = F) +
+  scale_x_continuous(labels = scales::comma, limits = c(ymin, ymax)) +
+  scale_color_manual("", values = pal) +
+  labs(title = "All Article Downloads",
+       subtitle = "By Article Domain",
+       x = "Number of Downloads", y = "") 
+
+
+plot_fn2 <- function(x, title){
+  x <- enquo(x)
+  
+  # get lables for plot
+  right_label <- els_group2 %>%
+    select(domain, provider2, !! x) %>% 
+    group_by(domain) %>%
+    arrange(desc(!! x)) %>%
+    top_n(1, wt = !! x)
+  left_label <- els_group2 %>%
+    select(domain, provider2, !! x) %>% 
+    group_by(domain) %>%
+    arrange(desc(!! x)) %>%
+    slice(2) %>% 
+    arrange(desc(!! x))
+
+  # set x-axis plot limits  
+  ymax <- els_group2 %>% summarise(ymax = max(!! x)) %>% pull() * 1.15
+  ymin <- els_group2 %>% summarise(ymin = min(!! x)) %>% pull() * -20
+  
+  # generate the plot
+  els_group2 %>% 
+    mutate(domain = fct_reorder(domain, !! x)) %>% 
+    ggplot(aes(x = !! x, y = domain)) +
+    geom_line(aes(group = domain)) +
+    geom_point(aes(color = provider2), size = 2) +
+    geom_text(data = right_label, aes(color = provider2, label = scales::comma(!! x)),
+              size = 3, hjust = -.5, show.legend = F) +
+    geom_text(data = left_label, aes(color = provider2, label = scales::comma(!! x)),
+              size = 3, hjust = 1.5, show.legend = F) +
+    scale_x_continuous(labels = scales::comma, limits = c(ymin, ymax)) +
+    scale_color_manual("", values = pal) +
+    labs(title = title,
+         subtitle = "By Article Domain",
+         x = "Number of Downloads", y = "") 
+}
+
+
+plot_fn2(jr1, title = "All 2017 Downloads from Elsevier Journals")
+plot_fn2(jr5, title = "Current Year 2017 Downloads from Elsevier Journals")
+plot_fn2(refs, title = "UVA References in Elsevier Journals")
+plot_fn2(pubs, title = "UVA-Authored Publications in Elsevier Journals")
