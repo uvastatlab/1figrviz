57d915c5716204a7fa8f9c87342612f5e1229391
Erich Purpur <epurpur@gmail.com>
2019-11-26 09:50:48 -0500

    Add files via upload

--- Python/Figure5.py
+++ Python/Figure5.py
@@ -34,7 +34,7 @@ def figure5b_papers():
     #this holds papers totals for all providers in the end, which is used to make final plot
     papers_by_provider = []
     
-    #populate references totals for elsevier subset providers    
+    #populate papers totals for elsevier subset providers    
     elsevier_providers = [elsevier_freedom_collection, elsevier_subscribed_titles]
     
     
@@ -68,7 +68,7 @@ def figure5b_papers():
         papers_by_provider.append(papers_by_year)
         
     
-    #populate references totals for other providers    
+    #populate papers totals for other providers    
     providers = ['Sage', 'Springer', 'Taylor & Francis', 'Wiley']
 
     for provider_name in providers:
@@ -104,7 +104,7 @@ def figure5b_papers():
     years = ['2008','2009','2010','2011','2012','2013','2014','2015','2016','2017']
 
     plt.figure(num=None, figsize=(10,10))
-    plt.suptitle(f'Number of Articles by {your_institution} Authors')
+    plt.suptitle(f'Number of Articles (papers) by {your_institution} Authors')
     plt.xlabel('Year')
     plt.ylabel('Number of Articles')
 
@@ -120,119 +120,62 @@ def figure5b_papers():
 
 
 
-#TODO: WHAT IS BEING COMPARED HERE?
+
 def figure5b_percentage():
     """ Show percent papers per year published by UVA authors as a percentage of all papers for each provider, separating Elsevier Freedom
-    and Elsevier Subscribed titles out from Elsevier as a whole."""
+    and Elsevier Subscribed titles out from Elsevier as a whole. All papers are the 'total papers in scopus' columns"""
     
     #    original_1figr_dataset = pd.read_excel(filename, sheet_name='Journals per Provider', skiprows=8)   
     original_1figr_dataset = pd.read_excel('JournalsPerProvider.xls', skiprows=8)
 
-    all_providers = original_1figr_dataset['Provider'].unique()     #makes list of unique providers
+    #holds percent papers per year published by UVA authors of total papers per provider
+    percent_papers_by_provider = []
 
 
-    #build total references for all providers by year
-    sum_2008 = 0
-    sum_2009 = 0
-    sum_2010 = 0
-    sum_2011 = 0
-    sum_2012 = 0
-    sum_2013 = 0
-    sum_2014 = 0
-    sum_2015 = 0
-    sum_2016 = 0
-    sum_2017 = 0
-    
-    
-    for provider_name in all_providers:
-        
-        subset_by_provider = original_1figr_dataset.loc[original_1figr_dataset['Provider'] == provider_name]
-
-        all_jr1_downloads_2008 = subset_by_provider['Downloads JR1 2017'].tolist()
-        sum_2008 += all_jr1_downloads_2008[0]
-        all_jr1_downloads_2009 = subset_by_provider['Downloads JR1 2017'].tolist()
-        sum_2009 += all_jr1_downloads_2009[0]
-        all_jr1_downloads_2010 = subset_by_provider['Downloads JR1 2017'].tolist()
-        sum_2010 += all_jr1_downloads_2010[0]
-        all_jr1_downloads_2011 = subset_by_provider['Downloads JR1 2017'].tolist()
-        sum_2011 += all_jr1_downloads_2011[0]
-        all_jr1_downloads_2012 = subset_by_provider['Downloads JR1 2017'].tolist()
-        sum_2012 += all_jr1_downloads_2012[0]
-        all_jr1_downloads_2013 = subset_by_provider['Downloads JR1 2017'].tolist()
-        sum_2013 += all_jr1_downloads_2013[0]
-        all_jr1_downloads_2014 = subset_by_provider['Downloads JR1 2017'].tolist()
-        sum_2014 += all_jr1_downloads_2014[0]
-        all_jr1_downloads_2015 = subset_by_provider['Downloads JR1 2017'].tolist()
-        sum_2015 += all_jr1_downloads_2015[0]
-        all_jr1_downloads_2016 = subset_by_provider['Downloads JR1 2017'].tolist()
-        sum_2016 += all_jr1_downloads_2016[0]
-        all_jr1_downloads_2017 = subset_by_provider['Downloads JR1 2017'].tolist()
-        sum_2017 += all_jr1_downloads_2017[0]
-
-    
-#    for provider_name in all_providers:
-#        
-#        subset_by_provider = original_1figr_dataset.loc[original_1figr_dataset['Provider'] == provider_name]
-#    
-#        papers_2008 = subset_by_provider[2008].tolist()
-#        sum_2008 += papers_2008[0]
-#        papers_2009 = subset_by_provider[2009].tolist()
-#        sum_2009 += papers_2009[0]
-#        papers_2010 = subset_by_provider[2010].tolist()
-#        sum_2010 += papers_2010[0]
-#        papers_2011 = subset_by_provider[2011].tolist()
-#        sum_2011 += papers_2011[0]
-#        papers_2012 = subset_by_provider[2012].tolist()
-#        sum_2012 += papers_2012[0]
-#        papers_2013 = subset_by_provider[2013].tolist()
-#        sum_2013 += papers_2013[0]
-#        papers_2014 = subset_by_provider[2014].tolist()
-#        sum_2014 += papers_2014[0]
-#        papers_2015 = subset_by_provider[2015].tolist()
-#        sum_2015 += papers_2015[0]
-#        papers_2016 = subset_by_provider[2016].tolist()
-#        sum_2016 += papers_2016[0]
-#        papers_2017 = subset_by_provider[2017].tolist()
-#        sum_2017 += papers_2017[0]
-        
-
-        
-    #build references by provider for each year
+    #populate papers totals for other providers    
     providers = ['Sage', 'Springer', 'Taylor & Francis', 'Wiley']
 
-    papers_by_provider = []    #this holds percentage of total references for each year by provider, which is later plotted
-    
     for provider_name in providers:
+
+        percent_papers_by_year = []
         
         subset_by_provider = original_1figr_dataset.loc[original_1figr_dataset['Provider'] == provider_name]
-
-        papers_by_year = []
     
         papers_2008 = subset_by_provider[2008].tolist()
-        papers_by_year.append(papers_2008[0]/sum_2008)
+        total_2008 = subset_by_provider['2008.4'].tolist()
+        percent_papers_by_year.append(sum(papers_2008)/sum(total_2008))
         papers_2009 = subset_by_provider[2009].tolist()
-        papers_by_year.append(papers_2009[0]/sum_2009)
+        total_2009 = subset_by_provider['2009.4'].tolist()
+        percent_papers_by_year.append(sum(papers_2009)/sum(total_2009))
         papers_2010 = subset_by_provider[2010].tolist()
-        papers_by_year.append(papers_2010[0]/sum_2010)
+        total_2010 = subset_by_provider['2010.4'].tolist()
+        percent_papers_by_year.append(sum(papers_2010)/sum(total_2010))
         papers_2011 = subset_by_provider[2011].tolist()
-        papers_by_year.append(papers_2011[0]/sum_2011)
+        total_2011 = subset_by_provider['2011.4'].tolist()
+        percent_papers_by_year.append(sum(papers_2011)/sum(total_2011)) 
         papers_2012 = subset_by_provider[2012].tolist()
-        papers_by_year.append(papers_2012[0]/sum_2012)
+        total_2012 = subset_by_provider['2012.4'].tolist()
+        percent_papers_by_year.append(sum(papers_2012)/sum(total_2012))
         papers_2013 = subset_by_provider[2013].tolist()
-        papers_by_year.append(papers_2013[0]/sum_2013)
+        total_2013 = subset_by_provider['2013.4'].tolist()
+        percent_papers_by_year.append(sum(papers_2013)/sum(total_2013)) 
         papers_2014 = subset_by_provider[2014].tolist()
-        papers_by_year.append(papers_2014[0]/sum_2014)
+        total_2014 = subset_by_provider['2014.4'].tolist()
+        percent_papers_by_year.append(sum(papers_2014)/sum(total_2014))
         papers_2015 = subset_by_provider[2015].tolist()
-        papers_by_year.append(papers_2015[0]/sum_2015)
+        total_2015 = subset_by_provider['2015.4'].tolist()
+        percent_papers_by_year.append(sum(papers_2015)/sum(total_2015))        
         papers_2016 = subset_by_provider[2016].tolist()
-        papers_by_year.append(papers_2016[0]/sum_2016)
+        total_2016 = subset_by_provider['2016.4'].tolist()
+        percent_papers_by_year.append(sum(papers_2016)/sum(total_2016)) 
         papers_2017 = subset_by_provider[2017].tolist()
-        papers_by_year.append(papers_2017[0]/sum_2017)   
+        total_2017 = subset_by_provider['2017.4'].tolist()
+        percent_papers_by_year.append(sum(papers_2017)/sum(total_2017))
         
-        papers_by_provider.append(papers_by_year)
-
-
-    #Calculate number of references for Elsevier Freedom and Elsevier Subscribed titles        
+        percent_papers_by_provider.append(percent_papers_by_year)
+        
+        
+    #populate papers totals for elsevier freedom and elsevier subscribed providers
     elsevier_freedom_collection = rf.make_freedom_collection_provider()
     elsevier_subscribed_titles = rf.make_elsevier_subscribed_titles_provider()
     
@@ -240,48 +183,61 @@ def figure5b_percentage():
     
     for provider_name in elsevier_providers:
 
-        papers_by_year = []
+        percent_papers_by_year = []
         
         papers_2008 = provider_name[2008].tolist()
-        papers_by_year.append((sum(papers_2008))/sum_2008)
+        total_2008 = provider_name['2008.4'].tolist()
+        percent_papers_by_year.append(sum(papers_2008)/sum(total_2008))
         papers_2009 = provider_name[2009].tolist()
-        papers_by_year.append((sum(papers_2009))/sum_2009)
+        total_2009 = provider_name['2009.4'].tolist()
+        percent_papers_by_year.append(sum(papers_2009)/sum(total_2009))
         papers_2010 = provider_name[2010].tolist()
-        papers_by_year.append((sum(papers_2010))/sum_2010)
+        total_2010 = provider_name['2010.4'].tolist()
+        percent_papers_by_year.append(sum(papers_2010)/sum(total_2010))
         papers_2011 = provider_name[2011].tolist()
-        papers_by_year.append((sum(papers_2011))/sum_2011)
+        total_2011 = provider_name['2011.4'].tolist()
+        percent_papers_by_year.append(sum(papers_2011)/sum(total_2011)) 
         papers_2012 = provider_name[2012].tolist()
-        papers_by_year.append((sum(papers_2012))/sum_2012)
+        total_2012 = provider_name['2012.4'].tolist()
+        percent_papers_by_year.append(sum(papers_2012)/sum(total_2012))
         papers_2013 = provider_name[2013].tolist()
-        papers_by_year.append((sum(papers_2013))/sum_2013)
+        total_2013 = provider_name['2013.4'].tolist()
+        percent_papers_by_year.append(sum(papers_2013)/sum(total_2013)) 
         papers_2014 = provider_name[2014].tolist()
-        papers_by_year.append((sum(papers_2014))/sum_2014)
+        total_2014 = provider_name['2014.4'].tolist()
+        percent_papers_by_year.append(sum(papers_2014)/sum(total_2014))
         papers_2015 = provider_name[2015].tolist()
-        papers_by_year.append((sum(papers_2015))/sum_2015)
+        total_2015 = provider_name['2015.4'].tolist()
+        percent_papers_by_year.append(sum(papers_2015)/sum(total_2015))        
         papers_2016 = provider_name[2016].tolist()
-        papers_by_year.append((sum(papers_2016))/sum_2016)
+        total_2016 = provider_name['2016.4'].tolist()
+        percent_papers_by_year.append(sum(papers_2016)/sum(total_2016)) 
         papers_2017 = provider_name[2017].tolist()
-        papers_by_year.append((sum(papers_2017))/sum_2017)
-        
-        papers_by_provider.append(papers_by_year)
+        total_2017 = provider_name['2017.4'].tolist()
+        percent_papers_by_year.append(sum(papers_2017)/sum(total_2017))
 
+        
+        percent_papers_by_provider.append(percent_papers_by_year)
+        
         
     years = ['2008','2009','2010','2011','2012','2013','2014','2015','2016','2017']
 
     plt.figure(num=None, figsize=(10,10))
-    plt.suptitle(f'Percent of All Articles by {your_institution} Authors')
+    plt.suptitle(f'Percent of All Articles published by {your_institution} Authors')
     plt.xlabel('Year')
     plt.ylabel('Percentage')
         
-#    plt.gca().yaxis.set_major_formatter(StrMethodFormatter('{x:,.0%}'))    #formats y axis as %
+    plt.gca().yaxis.set_major_formatter(StrMethodFormatter('{x:,.2%}'))    #formats y axis as %
 
-    plt.plot(years, papers_by_provider[0], label='Sage', color='blue')
-    plt.plot(years, papers_by_provider[1], label='Springer', color='green')
-    plt.plot(years, papers_by_provider[2], label='Taylor & Francis', color='purple')
-    plt.plot(years, papers_by_provider[3], label='Wiley', color='orange')
-    plt.plot(years, papers_by_provider[4], label='Elsevier Freedom', color='red', linestyle='dashed')
-    plt.plot(years, papers_by_provider[5], label='Elsevier Subscribed', color='red')
+    plt.plot(years, percent_papers_by_provider[0], label='Sage', color='blue')
+    plt.plot(years, percent_papers_by_provider[1], label='Springer', color='green')
+    plt.plot(years, percent_papers_by_provider[2], label='Taylor & Francis', color='purple')
+    plt.plot(years, percent_papers_by_provider[3], label='Wiley', color='orange')
+    plt.plot(years, percent_papers_by_provider[4], label='Elsevier Freedom', color='red', linestyle='dashed')
+    plt.plot(years, percent_papers_by_provider[5], label='Elsevier Subscribed', color='red')
         
     plt.legend(loc='center left', bbox_to_anchor=(1, 0.8))
     
-figure5b_percentage()  
+ 
+
+
--- Python/Figure6.py
+++ Python/Figure6.py
@@ -24,6 +24,7 @@ def figure6b_oa_available_articles():
     
 #    original_1figr_dataset = pd.read_excel(filename, sheet_name='Journals per Provider', skiprows=8)   
     original_1figr_dataset = pd.read_excel('JournalsPerProvider.xls', skiprows=8)
+
     elsevier_freedom_collection = rf.make_freedom_collection_provider()
     elsevier_subscribed_titles = rf.make_elsevier_subscribed_titles_provider()
 
@@ -109,148 +110,115 @@ def figure6b_oa_available_articles():
 
     plt.legend(loc='center left', bbox_to_anchor=(1, 0.8))
 
-    
 
-#TODO: WHAT ARE WE COMPARING HERE???
-def figure_6b_percent_oa_available_articles():
-    """Percent of All articles that are open access by provider by year. Separating elsevier subscribed and
-    elsevier freedom collection from elsevier as a whole"""
-    
-#    original_1figr_dataset = pd.read_excel(filename, sheet_name='Journals per Provider', skiprows=8)   
+
+def figure6b_percent_oa_articles():
+    """ Show percent papers per year published open access per provider per year as a percentage of all papers
+    published per provider per year. Separates elsevier freedom an elsevier subscribed titles out from the whole"""
+
+    #    original_1figr_dataset = pd.read_excel(filename, sheet_name='Journals per Provider', skiprows=8)   
     original_1figr_dataset = pd.read_excel('JournalsPerProvider.xls', skiprows=8)
+
+    #holds percent papers published open access per year per provider
+    percent_oa_papers_by_provider = []
+    
+    #populate % oa papers totals for other providers    
+    providers = ['Sage', 'Springer', 'Taylor & Francis', 'Wiley']
+
+    for provider_name in providers:
+
+        percent_oa_papers_by_year = []
+    
+        subset_by_provider = original_1figr_dataset.loc[original_1figr_dataset['Provider'] == provider_name]
+
+        oa_papers_2008 = subset_by_provider['2008.3'].tolist()
+        percent_oa_papers_by_year.append(oa_papers_2008[0])
+        oa_papers_2009 = subset_by_provider['2009.3'].tolist()
+        percent_oa_papers_by_year.append(oa_papers_2009[0])
+        oa_papers_2010 = subset_by_provider['2010.3'].tolist()
+        percent_oa_papers_by_year.append(oa_papers_2010[0])
+        oa_papers_2011 = subset_by_provider['2011.3'].tolist()
+        percent_oa_papers_by_year.append(oa_papers_2011[0])
+        oa_papers_2012 = subset_by_provider['2012.3'].tolist()
+        percent_oa_papers_by_year.append(oa_papers_2012[0])
+        oa_papers_2013 = subset_by_provider['2013.3'].tolist()
+        percent_oa_papers_by_year.append(oa_papers_2013[0])
+        oa_papers_2014 = subset_by_provider['2014.3'].tolist()
+        percent_oa_papers_by_year.append(oa_papers_2014[0])
+        oa_papers_2015 = subset_by_provider['2015.3'].tolist()
+        percent_oa_papers_by_year.append(oa_papers_2015[0])
+        oa_papers_2016 = subset_by_provider['2016.3'].tolist()
+        percent_oa_papers_by_year.append(oa_papers_2016[0])
+        oa_papers_2017 = subset_by_provider['2017.3'].tolist()
+        percent_oa_papers_by_year.append(oa_papers_2017[0])
+
+        percent_oa_papers_by_provider.append(percent_oa_papers_by_year)  
+        
+
+    #populate % oa papers totals for elsevier freedom and elsevier subscribed titles
+    #this is # of OA papers divided by total papers        
     elsevier_freedom_collection = rf.make_freedom_collection_provider()
     elsevier_subscribed_titles = rf.make_elsevier_subscribed_titles_provider()
-
-
-    all_providers = original_1figr_dataset['Provider'].unique()     #makes list of unique providers
-
-    #build total articles for all providers by year
-    sum_2008 = 0
-    sum_2009 = 0
-    sum_2010 = 0
-    sum_2011 = 0
-    sum_2012 = 0
-    sum_2013 = 0
-    sum_2014 = 0
-    sum_2015 = 0
-    sum_2016 = 0
-    sum_2017 = 0
-
-    for provider_name in all_providers:
-        
-        subset_by_provider = original_1figr_dataset.loc[original_1figr_dataset['Provider'] == provider_name]
-
-        all_oa_articles_2008 = subset_by_provider['2008.2'].tolist()
-        sum_2008 += all_oa_articles_2008[0]
-        all_oa_articles_2009 = subset_by_provider['2009.2'].tolist()
-        sum_2009 += all_oa_articles_2009[0]
-        all_oa_articles_2010 = subset_by_provider['2010.2'].tolist()
-        sum_2010 += all_oa_articles_2010[0]
-        all_oa_articles_2011 = subset_by_provider['2011.2'].tolist()
-        sum_2011 += all_oa_articles_2011[0]
-        all_oa_articles_2012 = subset_by_provider['2012.2'].tolist()
-        sum_2012 += all_oa_articles_2012[0]
-        all_oa_articles_2013 = subset_by_provider['2013.2'].tolist()
-        sum_2013 += all_oa_articles_2013[0]
-        all_oa_articles_2014 = subset_by_provider['2014.2'].tolist()
-        sum_2014 += all_oa_articles_2014[0]
-        all_oa_articles_2015 = subset_by_provider['2015.2'].tolist()
-        sum_2015 += all_oa_articles_2015[0]
-        all_oa_articles_2016 = subset_by_provider['2016.2'].tolist()
-        sum_2016 += all_oa_articles_2016[0]
-        all_oa_articles_2017 = subset_by_provider['2017.2'].tolist()
-        sum_2017 += all_oa_articles_2017[0]
-
-
-        
-    oa_articles_by_provider = []  #this holds percentage of OA articles by provider
-
-    #Calculate number of references for Elsevier Freedom and Elsevier Subscribed titles            
-    elsevier_providers = [elsevier_freedom_collection, elsevier_subscribed_titles]    
-
-    for provider_name in elsevier_providers:
-        
-        oa_articles_by_year = []
-        
-        oa_articles_2008 = provider_name['2008.2'].tolist()
-        oa_articles_by_year.append(sum(oa_articles_2008)/sum_2008)
-        oa_articles_2009 = provider_name['2009.2'].tolist()
-        oa_articles_by_year.append(sum(oa_articles_2009)/sum_2009)
-        oa_articles_2010 = provider_name['2010.2'].tolist()
-        oa_articles_by_year.append(sum(oa_articles_2010)/sum_2010)
-        oa_articles_2011 = provider_name['2011.2'].tolist()
-        oa_articles_by_year.append(sum(oa_articles_2011)/sum_2011)
-        oa_articles_2012 = provider_name['2012.2'].tolist()
-        oa_articles_by_year.append(sum(oa_articles_2012)/sum_2012)
-        oa_articles_2013 = provider_name['2013.2'].tolist()
-        oa_articles_by_year.append(sum(oa_articles_2013)/sum_2013)
-        oa_articles_2014 = provider_name['2014.2'].tolist()
-        oa_articles_by_year.append(sum(oa_articles_2014)/sum_2014)
-        oa_articles_2015 = provider_name['2015.2'].tolist()
-        oa_articles_by_year.append(sum(oa_articles_2015)/sum_2015)
-        oa_articles_2016 = provider_name['2016.2'].tolist()
-        oa_articles_by_year.append(sum(oa_articles_2016)/sum_2016)
-        oa_articles_2017 = provider_name['2017.2'].tolist()
-        oa_articles_by_year.append(sum(oa_articles_2017)/sum_2017)
-        
-        oa_articles_by_provider.append(oa_articles_by_year)
-        
-    #build percentage of open access articles by provider for each year
-    providers = ['Sage', 'Springer', 'Taylor & Francis', 'Wiley']
     
-    for provider_name in providers:
-        
-        subset_by_provider = original_1figr_dataset.loc[original_1figr_dataset['Provider'] == provider_name]
+    elsevier_providers = [elsevier_freedom_collection, elsevier_subscribed_titles]
+    
+    for provider_name in elsevier_providers:
 
-        oa_articles_by_year = []
+        percent_oa_papers_by_year = []
         
-        oa_articles_2008 = subset_by_provider['2008.2'].tolist()
-        oa_articles_by_year.append(oa_articles_2008[0]/sum_2008)  
-        oa_articles_2009 = subset_by_provider['2009.2'].tolist()
-        oa_articles_by_year.append(oa_articles_2009[0]/sum_2009)  
-        oa_articles_2010 = subset_by_provider['2010.2'].tolist()
-        oa_articles_by_year.append(oa_articles_2010[0]/sum_2010)  
-        oa_articles_2011 = subset_by_provider['2011.2'].tolist()
-        oa_articles_by_year.append(oa_articles_2011[0]/sum_2011)  
-        oa_articles_2012 = subset_by_provider['2012.2'].tolist()
-        oa_articles_by_year.append(oa_articles_2012[0]/sum_2012)  
-        oa_articles_2013 = subset_by_provider['2013.2'].tolist()
-        oa_articles_by_year.append(oa_articles_2013[0]/sum_2013)  
-        oa_articles_2014 = subset_by_provider['2014.2'].tolist()
-        oa_articles_by_year.append(oa_articles_2014[0]/sum_2014)  
-        oa_articles_2015 = subset_by_provider['2015.2'].tolist()
-        oa_articles_by_year.append(oa_articles_2015[0]/sum_2015)  
-        oa_articles_2016 = subset_by_provider['2016.2'].tolist()
-        oa_articles_by_year.append(oa_articles_2016[0]/sum_2016)  
-        oa_articles_2017 = subset_by_provider['2017.2'].tolist()
-        oa_articles_by_year.append(oa_articles_2017[0]/sum_2017)
+        oa_papers_2008 = provider_name['2008.2'].tolist()
+        total_2008 = provider_name['2008.4'].tolist()
+        percent_oa_papers_by_year.append(sum(oa_papers_2008)/sum(total_2008))
+        oa_papers_2009 = provider_name['2009.2'].tolist()
+        total_2009 = provider_name['2009.4'].tolist()
+        percent_oa_papers_by_year.append(sum(oa_papers_2009)/sum(total_2009))
+        oa_papers_2010 = provider_name['2010.2'].tolist()
+        total_2010 = provider_name['2010.4'].tolist()
+        percent_oa_papers_by_year.append(sum(oa_papers_2010)/sum(total_2010))
+        oa_papers_2011 = provider_name['2010.2'].tolist()
+        total_2011 = provider_name['2011.4'].tolist()
+        percent_oa_papers_by_year.append(sum(oa_papers_2011)/sum(total_2011))
+        oa_papers_2012 = provider_name['2012.2'].tolist()
+        total_2012 = provider_name['2012.4'].tolist()
+        percent_oa_papers_by_year.append(sum(oa_papers_2012)/sum(total_2012))
+        oa_papers_2013 = provider_name['2013.2'].tolist()
+        total_2013 = provider_name['2013.4'].tolist()
+        percent_oa_papers_by_year.append(sum(oa_papers_2013)/sum(total_2013))
+        oa_papers_2014 = provider_name['2014.2'].tolist()
+        total_2014 = provider_name['2014.4'].tolist()
+        percent_oa_papers_by_year.append(sum(oa_papers_2014)/sum(total_2014))
+        oa_papers_2015 = provider_name['2015.2'].tolist()
+        total_2015 = provider_name['2015.4'].tolist()
+        percent_oa_papers_by_year.append(sum(oa_papers_2015)/sum(total_2015))
+        oa_papers_2016 = provider_name['2016.2'].tolist()
+        total_2016 = provider_name['2016.4'].tolist()
+        percent_oa_papers_by_year.append(sum(oa_papers_2016)/sum(total_2016))
+        oa_papers_2017 = provider_name['2017.2'].tolist()
+        total_2017 = provider_name['2017.4'].tolist()
+        percent_oa_papers_by_year.append(sum(oa_papers_2017)/sum(total_2017))
         
-        oa_articles_by_provider.append(oa_articles_by_year)
-
+        percent_oa_papers_by_provider.append(percent_oa_papers_by_year)
 
     years = ['2008','2009','2010','2011','2012','2013','2014','2015','2016','2017']
 
     plt.figure(num=None, figsize=(10,10))
-    plt.suptitle(f'Percent of OA-Available Articles')
+    plt.suptitle(f'Percent of all Open Access by provider')
     plt.xlabel('Year')
     plt.ylabel('Percentage')
+        
+    plt.gca().yaxis.set_major_formatter(StrMethodFormatter('{x:,.0%}'))    #formats y axis as %
 
-
-    plt.plot(years, oa_articles_by_provider[0], label='Elsevier Freedom', color='red', linestyle='dashed')
-    plt.plot(years, oa_articles_by_provider[1], label='Elsevier Subscribed', color='red')
-    plt.plot(years, oa_articles_by_provider[2], label='Sage', color='blue')
-    plt.plot(years, oa_articles_by_provider[3], label='Springer', color='green')
-    plt.plot(years, oa_articles_by_provider[4], label='Taylor & Francis', color='purple')
-    plt.plot(years, oa_articles_by_provider[5], label='Wiley', color='orange')
-
+    plt.plot(years, percent_oa_papers_by_provider[0], label='Sage', color='blue')
+    plt.plot(years, percent_oa_papers_by_provider[1], label='Springer', color='green')
+    plt.plot(years, percent_oa_papers_by_provider[2], label='Taylor & Francis', color='purple')
+    plt.plot(years, percent_oa_papers_by_provider[3], label='Wiley', color='orange')
+    plt.plot(years, percent_oa_papers_by_provider[4], label='Elsevier Freedom', color='red', linestyle='dashed')
+    plt.plot(years, percent_oa_papers_by_provider[5], label='Elsevier Subscribed', color='red')
+        
     plt.legend(loc='center left', bbox_to_anchor=(1, 0.8))
 
 
-    
-figure_6b_percent_oa_available_articles()    
-
-
-
+figure6b_percent_oa_articles()
 
 
 
