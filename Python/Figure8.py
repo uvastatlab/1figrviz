eff716020b40b67883a0373264824a0236505154
Erich Purpur <epurpur@gmail.com>
2019-11-28 11:00:29 -0500

    Add files via upload

--- Python/Figure8.py
+++ Python/Figure8.py
@@ -27,23 +27,55 @@ def figure8e():
     original_1figr_data_with_disciplines = rf.make_disciplines_column()
     subscribed_titles_provider = rf.make_elsevier_subscribed_titles_with_disciplines(original_1figr_data_with_disciplines)
     freedom_collection_provider = rf.make_freedom_collection_provider_with_disciplines(original_1figr_data_with_disciplines)
-     
-    unique_disciplines = original_1figr_data_with_disciplines['Discipline'].unique()
 
+    unique_disciplines = original_1figr_data_with_disciplines['Discipline'].unique().tolist()
+
+    for i in unique_disciplines:
+        if type(i) == float:
+            unique_disciplines.remove(i)
+    unique_disciplines.sort()
+
+    stats_by_discipline = []
+
+    for discipline in unique_disciplines:
+        
+        subscribed_subset_by_discipline = subscribed_titles_provider.loc[subscribed_titles_provider['Discipline'] == discipline]
+        subscribed_jr1_downloads = subscribed_subset_by_discipline['Downloads JR1 2017'].sum()
+
+        freedom_subset_by_discipline = freedom_collection_provider.loc[freedom_collection_provider['Discipline'] == discipline]
+        freedom_jr1_downloads = freedom_subset_by_discipline['Downloads JR1 2017'].sum()
+        
+        stats_by_discipline.append((discipline, subscribed_jr1_downloads, freedom_jr1_downloads))
+            
+    #sorts disciplines by sum of total # of jr1 downloads
+    arrangement = sorted(stats_by_discipline, key=lambda x: (x[1] + x[2]), reverse=True) 
     
-    jr1_downloads_by_provider = []
+    #splitting elements of tuples in arrangement into lists so it is easier to plot
+    disciplines_list = [i[0] for i in arrangement]
+    subscribed_total = [i[1] for i in arrangement]
+    freedom_total = [i[2] for i in arrangement]
+
+
+#####FIX PLOT
+    mpl.rcParams['ytick.major.width'] = 1
+    mpl.rcParams['xtick.major.width'] = 1
+    plt.figure(num=None, figsize=(8,8))
+    plt.suptitle(f'JR1 downloads by Discipline')
+    plot = plt.bar(disciplines_list, subscribed_total, width=.3, color='green')   
+    plt.bar(disciplines_list, freedom_total, width=.3, color='orange')
     
-    #count of JR1 downloads by discipline for subscribed titles provider
-    subscribed_titles_disciplines = subscribed_titles_provider.groupby('Discipline')
-    count_list = subscribed_titles_disciplines['Downloads JR1 2017'].sum().tolist()
-    jr1_downloads_by_provider.append(count_list)
+    plt.xticks(rotation=90)
     
-    #count of JR1 downloads by discipline for freedom collection provider
-    freedom_collection_disciplines = freedom_collection_provider.groupby('Discipline')
-    count_list = freedom_collection_disciplines['Downloads JR1 2017'].sum().tolist()
-    jr1_downloads_by_provider.append(count_list)
+    for i in plot:
+        score = i.get_height()
+        plt.text(i.get_x(),                       #y axis location
+                 score,                           #x axis location
+                 '{0:g}'.format(float(score)))    #format floats without decimals
     
-#TODO: get list of disciplines, plot count per discipline
+       
+
+figure8e()
+
     
 
 def figure8f():
