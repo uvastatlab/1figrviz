be93f99579e662eb080d4ae91ce582bfd2cca1d7
Erich Purpur <epurpur@gmail.com>
2019-12-01 17:25:49 -0500

    Add files via upload

--- Python/Figure8.py
+++ Python/Figure8.py
@@ -48,7 +48,7 @@ def figure8e():
         stats_by_discipline.append((discipline, subscribed_jr1_downloads, freedom_jr1_downloads))
             
     #sorts disciplines by sum of total # of jr1 downloads
-    arrangement = sorted(stats_by_discipline, key=lambda x: (x[1] + x[2]), reverse=True) 
+    arrangement = sorted(stats_by_discipline, key=lambda x: (x[1] + x[2])) 
     
     #splitting elements of tuples in arrangement into lists so it is easier to plot
     disciplines_list = [i[0] for i in arrangement]
@@ -56,26 +56,19 @@ def figure8e():
     freedom_total = [i[2] for i in arrangement]
 
 
-#####FIX PLOT
     mpl.rcParams['ytick.major.width'] = 1
     mpl.rcParams['xtick.major.width'] = 1
     plt.figure(num=None, figsize=(8,8))
     plt.suptitle(f'JR1 downloads by Discipline')
-    plot = plt.bar(disciplines_list, subscribed_total, width=.3, color='green')   
-    plt.bar(disciplines_list, freedom_total, width=.3, color='orange')
-    
-    plt.xticks(rotation=90)
-    
-    for i in plot:
-        score = i.get_height()
-        plt.text(i.get_x(),                       #y axis location
-                 score,                           #x axis location
-                 '{0:g}'.format(float(score)))    #format floats without decimals
+ 
+    plot = plt.scatter(subscribed_total, disciplines_list, color='blue')
+    plot2 = plt.scatter(freedom_total, disciplines_list, color='orange')
+
+    subscribed_legend_label = mpatches.Patch(color='blue', label='Elsevier Subscribed')
+    freedom_legend_label = mpatches.Patch(color='orange', label='Elsevier Freedom')
+    plt.legend(loc='lower right', handles=[subscribed_legend_label, freedom_legend_label])
     
        
-
-figure8e()
-
     
 
 def figure8f():
@@ -88,23 +81,45 @@ def figure8f():
     subscribed_titles_provider = rf.make_elsevier_subscribed_titles_with_disciplines(original_1figr_data_with_disciplines)
     freedom_collection_provider = rf.make_freedom_collection_provider_with_disciplines(original_1figr_data_with_disciplines)
      
-    unique_disciplines = original_1figr_data_with_disciplines['Discipline'].unique()
+    unique_disciplines = original_1figr_data_with_disciplines['Discipline'].unique().tolist()
 
-    
-    jr5_downloads_by_provider = []
-    
-    #count of JR5 downloads by discipline for subscribed titles provider
-    subscribed_titles_disciplines = subscribed_titles_provider.groupby('Discipline')
-    count_list = subscribed_titles_disciplines['Downloads JR5 2017 in 2017'].sum().tolist()
-    jr5_downloads_by_provider.append(count_list)
-    
-    #count of JR5 downloads by discipline for freedom collection provider
-    freedom_collection_disciplines = freedom_collection_provider.groupby('Discipline')
-    count_list = freedom_collection_disciplines['Downloads JR5 2017 in 2017'].sum().tolist()
-    jr5_downloads_by_provider.append(count_list)
+    for i in unique_disciplines:
+        if type(i) == float:
+            unique_disciplines.remove(i)
+    unique_disciplines.sort()
 
-#TODO: get list of disciplines, plot count per discipline
+    stats_by_discipline = []
 
+    for discipline in unique_disciplines:
+        
+        subscribed_subset_by_discipline = subscribed_titles_provider.loc[subscribed_titles_provider['Discipline'] == discipline]
+        subscribed_jr5_downloads = subscribed_subset_by_discipline['Downloads JR5 2017 in 2017'].sum()
+
+        freedom_subset_by_discipline = freedom_collection_provider.loc[freedom_collection_provider['Discipline'] == discipline]
+        freedom_jr5_downloads = freedom_subset_by_discipline['Downloads JR5 2017 in 2017'].sum()
+        
+        stats_by_discipline.append((discipline, subscribed_jr5_downloads, freedom_jr5_downloads))
+            
+    #sorts disciplines by sum of total # of jr5 downloads
+    arrangement = sorted(stats_by_discipline, key=lambda x: (x[1] + x[2])) 
+    
+    #splitting elements of tuples in arrangement into lists so it is easier to plot
+    disciplines_list = [i[0] for i in arrangement]
+    subscribed_total = [i[1] for i in arrangement]
+    freedom_total = [i[2] for i in arrangement]
+
+
+    mpl.rcParams['ytick.major.width'] = 1
+    mpl.rcParams['xtick.major.width'] = 1
+    plt.figure(num=None, figsize=(8,8))
+    plt.suptitle(f'JR5 downloads by Discipline')
+ 
+    plot = plt.scatter(subscribed_total, disciplines_list, color='blue')
+    plot2 = plt.scatter(freedom_total, disciplines_list, color='orange')
+
+    subscribed_legend_label = mpatches.Patch(color='blue', label='Elsevier Subscribed')
+    freedom_legend_label = mpatches.Patch(color='orange', label='Elsevier Freedom')
+    plt.legend(loc='lower right', handles=[subscribed_legend_label, freedom_legend_label])
     
 
 def figure8g():
@@ -117,22 +132,47 @@ def figure8g():
     subscribed_titles_provider = rf.make_elsevier_subscribed_titles_with_disciplines(original_1figr_data_with_disciplines)
     freedom_collection_provider = rf.make_freedom_collection_provider_with_disciplines(original_1figr_data_with_disciplines)
      
-    unique_disciplines = original_1figr_data_with_disciplines['Discipline'].unique()
+    unique_disciplines = original_1figr_data_with_disciplines['Discipline'].unique().tolist()
 
+    for i in unique_disciplines:
+        if type(i) == float:
+            unique_disciplines.remove(i)
+    unique_disciplines.sort()
+
+    stats_by_discipline = []
+
+    for discipline in unique_disciplines:
+        
+        subscribed_subset_by_discipline = subscribed_titles_provider.loc[subscribed_titles_provider['Discipline'] == discipline]
+        subscribed_jr5_downloads = subscribed_subset_by_discipline['References'].sum()
+
+        freedom_subset_by_discipline = freedom_collection_provider.loc[freedom_collection_provider['Discipline'] == discipline]
+        freedom_jr5_downloads = freedom_subset_by_discipline['References'].sum()
+        
+        stats_by_discipline.append((discipline, subscribed_jr5_downloads, freedom_jr5_downloads))
+            
+    #sorts disciplines by sum of total # of jr5 downloads
+    arrangement = sorted(stats_by_discipline, key=lambda x: (x[1] + x[2])) 
     
-    references_by_provider = []
-    
-    #count of JR5 downloads by discipline for subscribed titles provider
-    subscribed_titles_disciplines = subscribed_titles_provider.groupby('Discipline')
-    count_list = subscribed_titles_disciplines['References'].sum().tolist()
-    references_by_provider.append(count_list)
-    
-    #count of JR5 downloads by discipline for freedom collection provider
-    freedom_collection_disciplines = freedom_collection_provider.groupby('Discipline')
-    count_list = freedom_collection_disciplines['References'].sum().tolist()
-    references_by_provider.append(count_list)
-    
-#TODO: get list of disciplines, plot count per discipline
+    #splitting elements of tuples in arrangement into lists so it is easier to plot
+    disciplines_list = [i[0] for i in arrangement]
+    subscribed_total = [i[1] for i in arrangement]
+    freedom_total = [i[2] for i in arrangement]
+
+
+    mpl.rcParams['ytick.major.width'] = 1
+    mpl.rcParams['xtick.major.width'] = 1
+    plt.figure(num=None, figsize=(8,8))
+    plt.suptitle(f'References by Discipline')
+ 
+    plot = plt.scatter(subscribed_total, disciplines_list, color='blue')
+    plot2 = plt.scatter(freedom_total, disciplines_list, color='orange')
+
+    subscribed_legend_label = mpatches.Patch(color='blue', label='Elsevier Subscribed')
+    freedom_legend_label = mpatches.Patch(color='orange', label='Elsevier Freedom')
+    plt.legend(loc='lower right', handles=[subscribed_legend_label, freedom_legend_label])
+
+
 
 
 def figure8h():
@@ -140,26 +180,50 @@ def figure8h():
     Then splits elsevier into elsevier freedom provider and elsevier subscribed titles provider
     
     This is counting papers by discipline for the subscribed titles and freedom collection providers"""
+
     original_1figr_data_with_disciplines = rf.make_disciplines_column()
     subscribed_titles_provider = rf.make_elsevier_subscribed_titles_with_disciplines(original_1figr_data_with_disciplines)
     freedom_collection_provider = rf.make_freedom_collection_provider_with_disciplines(original_1figr_data_with_disciplines)
      
-    unique_disciplines = original_1figr_data_with_disciplines['Discipline'].unique()
+    unique_disciplines = original_1figr_data_with_disciplines['Discipline'].unique().tolist()
 
-    
-    papers_by_provider = []
-    
-    #count of JR5 downloads by discipline for subscribed titles provider
-    subscribed_titles_disciplines = subscribed_titles_provider.groupby('Discipline')
-    count_list = subscribed_titles_disciplines['Papers'].sum().tolist()
-    papers_by_provider.append(count_list)
-    
-    #count of JR5 downloads by discipline for freedom collection provider
-    freedom_collection_disciplines = freedom_collection_provider.groupby('Discipline')
-    count_list = freedom_collection_disciplines['Papers'].sum().tolist()
-    papers_by_provider.append(count_list)
+    for i in unique_disciplines:
+        if type(i) == float:
+            unique_disciplines.remove(i)
+    unique_disciplines.sort()
 
-#TODO: get list of disciplines, plot count per discipline
+    stats_by_discipline = []
+
+    for discipline in unique_disciplines:
+        
+        subscribed_subset_by_discipline = subscribed_titles_provider.loc[subscribed_titles_provider['Discipline'] == discipline]
+        subscribed_jr5_downloads = subscribed_subset_by_discipline['Papers'].sum()
+
+        freedom_subset_by_discipline = freedom_collection_provider.loc[freedom_collection_provider['Discipline'] == discipline]
+        freedom_jr5_downloads = freedom_subset_by_discipline['Papers'].sum()
+        
+        stats_by_discipline.append((discipline, subscribed_jr5_downloads, freedom_jr5_downloads))
+            
+    #sorts disciplines by sum of total # of jr5 downloads
+    arrangement = sorted(stats_by_discipline, key=lambda x: (x[1] + x[2])) 
+    
+    #splitting elements of tuples in arrangement into lists so it is easier to plot
+    disciplines_list = [i[0] for i in arrangement]
+    subscribed_total = [i[1] for i in arrangement]
+    freedom_total = [i[2] for i in arrangement]
+
+
+    mpl.rcParams['ytick.major.width'] = 1
+    mpl.rcParams['xtick.major.width'] = 1
+    plt.figure(num=None, figsize=(8,8))
+    plt.suptitle(f'Papers (Published Articles) by Discipline')
+ 
+    plot = plt.scatter(subscribed_total, disciplines_list, color='blue')
+    plot2 = plt.scatter(freedom_total, disciplines_list, color='orange')
+
+    subscribed_legend_label = mpatches.Patch(color='blue', label='Elsevier Subscribed')
+    freedom_legend_label = mpatches.Patch(color='orange', label='Elsevier Freedom')
+    plt.legend(loc='lower right', handles=[subscribed_legend_label, freedom_legend_label])
     
 
 
